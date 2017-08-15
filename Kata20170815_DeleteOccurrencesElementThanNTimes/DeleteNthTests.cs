74cd766cec5766e082c158b0d59f6c9612bc3abb
cash <cash@cashwu.com>
2017-08-15 22:09:00 +0800

    input 1,2,1 and 1 time test case

--- Kata20170815_DeleteOccurrencesElementThanNTimes/DeleteNthTests.cs
+++ Kata20170815_DeleteOccurrencesElementThanNTimes/DeleteNthTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Kata20170815_DeleteOccurrencesElementThanNTimes
@@ -7,8 +9,24 @@ namespace Kata20170815_DeleteOccurrencesElementThanNTimes
     public class DeleteNthTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void input_1_2_1_and_1_should_return_1_2()
         {
+            DeleteNthShouldBe(new[] {1, 2}, new[] {1, 2, 1}, 1);
+        }
+
+        private static void DeleteNthShouldBe(int[] expected, int[] array, int time)
+        {
+            var kata = new Kata();
+            var actual = kata.DeleteNth(array, time);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+
+    public class Kata
+    {
+        public int[] DeleteNth(int[] arr, int x)
+        {
+            return arr.Distinct().ToArray();
         }
     }
 }
